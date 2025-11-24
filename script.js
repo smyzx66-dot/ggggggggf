f6b1b8ffc05079afcd7aceb25bb7c949360f6765
smyzx66-dot <smyzx66@gmail.com>
2025-11-24 20:26:53 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+[
+  {
+    "id": 1,
+    "name": "أ. علي محمد",
+    "subject": "الرياضيات",
+    "image": "https://cdn-icons-png.flaticon.com/512/1995/1995515.png",
+    "lectures": [
+      { "id": 101, "title": "المحاضرة 1: الأعداد المركبة", "url": "https://youtube.com" },
+      { "id": 102, "title": "المحاضرة 2: القطوع المخروطية", "url": "https://google.com" }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "د. سارة أحمد",
+    "subject": "الفيزياء",
+    "image": "https://cdn-icons-png.flaticon.com/512/2038/2038687.png",
+    "lectures": [
+      { "id": 201, "title": "المحاضرة 1: المتسعات", "url": "#" },
+      { "id": 202, "title": "المحاضرة 2: الحث الكهرومغناطيسي", "url": "#" },
+      { "id": 203, "title": "المحاضرة 3: التيار المتناوب", "url": "#" }
+    ]
+  }
+]
